ccd331276555a1abce239d3b05e114beb59ac070
SubZer0811 <subashmylraj@gmail.com>
2023-02-16 18:11:25 +0530

    Testing in progress

---
+++ corr_fft_v3_1.py
@@ -0,0 +1,110 @@
+import numpy as np
+import corr_helper
+import helper
+
+def complex_mult(A, B):
+	a = A.real
+	b = A.imag
+	c = B.real
+	d = B.imag
+
+	return (a*c - b*d) + (a*d + b*c)*1j
+
+def compute_w(sig_len):
+	W = [[0]*sig_len for i in range(sig_len)]
+	W_I = [[0]*sig_len for i in range(sig_len)]
+
+	for i in range(0, sig_len):
+		for j in range(1, sig_len):
+			W_t = np.power(np.power(np.e, -1j * np.pi / j), i)
+			W[i][j] = np.sign(W_t.real) * np.power(2,np.round(np.log2(abs(W_t.real)))) + (np.sign(W_t.imag) * np.power(2,np.round(np.log2(abs(W_t.imag)))))*1j
+			W_I_t = np.power(np.power(np.e, 1j * np.pi / j), i)
+			W_I[i][j] = np.sign(W_I_t.real) * np.power(2,np.round(np.log2(abs(W_I_t.real)))) + (np.sign(W_I_t.imag) * np.power(2,np.round(np.log2(abs(W_I_t.imag)))))*1j
+			# W[i][j] = (i,j)
+
+	return (W, W_I)
+
+def FFT_1D(signal, W):
+
+	signal_f = [signal[int(format(str(bin(i))[:1:-1], f'0{int(np.log2(len(signal)))}s'), base=2)] for i in range(len(signal))]
+	signal_f = np.asarray(signal_f, dtype=np.complex)
+	
+	n = len(signal)
+	for s in range(1, int(np.log2(n))+1):
+		m = 1 << s
+		m2 = m >> 1
+		
+		for j in range(m2):
+			for k in range(j, n, m):
+
+				# t = W[j][m2] * signal_f[k + m2]
+				t = W[j][m2] * signal_f[k + m2]
+				u = signal_f[k]
+
+				signal_f[k] = u + t
+				signal_f[k + m2] = u - t
+
+	return signal_f
+
+def IFFT_1D(signal_f, W_I):
+	n = len(signal_f)
+	signal = [i/n for i in signal_f]
+
+
+	for s in range(int(np.log2(n)), 0, -1):
+		m = pow(2,s)	# interval between butterflies
+		m2 = m >> 1		# size of butterfly
+
+		for j in range(m2):
+			for k in range(j, n, m):
+
+				u = signal[k]
+				v = signal[k + m2]
+
+				signal[k] = u + v
+				# signal[k + m2] = (u - v) * W_I[j][m2]
+				signal[k + m2] = (u - v) * W_I[j][m2]
+
+	signal = [signal[int(format(str(bin(i))[:1:-1], f'0{int(np.log2(len(signal)))}s'), base=2)] for i in range(len(signal))]
+
+	return signal
+
+def FFT_2D(signal, W):
+
+	rows_FFT = []
+	for i in signal:
+		rows_FFT.append(FFT_1D(i, W))
+	
+	cols_FFT = np.asarray(rows_FFT).T
+	signal_f = []
+	for i in cols_FFT:
+		signal_f.append(FFT_1D(i, W))
+	return np.asarray(signal_f).T
+
+def IFFT_2D(signal_f, W_I):
+
+	rows_FFT = []
+	for i in signal_f:
+		rows_FFT.append(IFFT_1D(i, W_I))
+	
+	cols_FFT = np.asarray(rows_FFT).T
+	signal = []
+	for i in cols_FFT:
+		signal.append(IFFT_1D(i, W_I))
+	return np.asarray(signal).T
+
+def corr(img, kern, W, W_I, unpad=None):
+	res = IFFT_2D(FFT_2D(img, W) * FFT_2D(kern, W).conj(), W_I)
+
+	if unpad:
+		res = corr_helper.fft_unpad(res, unpad)
+
+	return res
+
+if __name__ == "__main__":
+
+	img, kern = corr_helper.gen_img_kern((100, 100), (5,5), F_SIZE=128)
+
+	W, W_I = compute_w(128)
+	print(np.allclose(img, IFFT_2D(FFT_2D(img, W), W_I)))
+
--- main.py
+++ main.py
@@ -7,12 +7,13 @@ import corr_brute
 import corr_fft_v1
 import corr_fft_v2
 import corr_fft_v3
+import corr_fft_v3_1
 import corr_helper
 
 import helper
 
-F_SIZE = 32
-IMG_SIZE = 14
+F_SIZE = 128
+IMG_SIZE = 64
 K_SIZE = 7
 
 img, kern = corr_helper.gen_img_kern((IMG_SIZE,IMG_SIZE),(K_SIZE,K_SIZE))
@@ -20,25 +21,20 @@ img, kern = corr_helper.gen_img_kern((IMG_SIZE,IMG_SIZE),(K_SIZE,K_SIZE))
 img = cv2.imread('lena.png', 0)
 img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
 
-img_p, kern_p = corr_helper.pad_img_kern(img, kern, F_SIZE)
 
 [W, W_I] = corr_fft_v3.compute_w(F_SIZE)
+[W_1, W_I_1] = corr_fft_v3_1.compute_w(F_SIZE)
+print(cv2.PSNR(np.asarray(W).real, np.asarray(W_1).real))
+print(cv2.PSNR(np.asarray(W).imag, np.asarray(W_1).imag))
+# exit(0)
+
+img_p, kern_p = corr_helper.pad_img_kern(img, kern, F_SIZE)
 
 out_b = corr_brute.corr(img_p, kern, img.shape[0]+kern.shape[0]-1)
-out_v3 = corr_fft_v3.corr(img_p, kern_p, W, W_I, (IMG_SIZE, K_SIZE))
-out_v2 = corr_fft_v2.corr(img_p, kern_p, (IMG_SIZE, K_SIZE))
-out_v1 = corr_fft_v1.corr(img_p, kern_p, (IMG_SIZE, K_SIZE))
+out_v3_1 = corr_fft_v3_1.corr(img_p, kern_p, W_1, W_I_1, (IMG_SIZE, K_SIZE))
 
-# print(out_v3.shape, out_b.shape)
-# helper.view_image("Original", img_p)
-# helper.view_image("Kernel", kern_p)
-# helper.view_image("brute", out_b)
-# helper.view_image("v3.real", out_v3.real)
-# # helper.view_image("v1", out_v1.real)
-# # helper.view_image("v3.imag", out_v3.imag)
-# helper.view_image("diff", out_v3.real - out_b)
+helper.view_image("brute", out_b)
+helper.view_image("v3_1", out_v3_1.real)
 
-print(cv2.PSNR(np.float32(out_v3.real), np.float32(out_b)))
-print(cv2.PSNR(np.float32(out_v2.real), np.float32(out_b)))
-print(cv2.PSNR(np.float32(out_v1.real), np.float32(out_b)))
-print(np.sqrt(np.mean((out_v3.real-out_b)**2)))
+print(cv2.PSNR(np.float32(out_v3_1.real), np.float32(out_b)))
+print(np.sqrt(np.mean((out_v3_1.real-out_b)**2)))
